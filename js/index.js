7e5b1762d23fe0922f1ad8afa37c12fbc48ccf3e
daniel kafer <daniel.j.kafer@gmail.com>
2016-11-15 20:41:18 -0600

    Added person list as a stateful component

--- js/index.js
+++ js/index.js
@@ -3,8 +3,8 @@ require('babel-polyfill');
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = require('./components/person');
+var PersonList = require('./components/person_list');
 
 document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(<Person />, document.getElementById('app'));
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
 });
